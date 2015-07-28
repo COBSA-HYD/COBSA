9402b9e47edaa2a64fea4d7dd95106228b447272
Paulcy Mboungou <paulcy.mboungou@gmail.com>
2015-07-28 16:39:43 +0530

    Paulcy adding index.

--- COBSA/views.py
+++ COBSA/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from django.template import RequestContext, loader
 
-# Create your views here.
+def index(request):
+	template = loader.get_template('COBSA/index.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
