74374cae72500a71956b4d721dc221af9851fe55
Chanservain IBAKA <chanservain.ibaka@gmail.com>
2015-08-29 20:55:59 +0530

    Modified links between home page and photos

--- COBSA/views.py
+++ COBSA/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def index(request):
-	template = loader.get_template('COBSA/index.html')
+	template = loader.get_template('COBSA/Home.html')
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
